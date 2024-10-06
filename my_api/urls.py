c979961e2374b490fd89b9a04d1533909e212119
NguyenCoderVN <41292507+NguyenCoderVN@users.noreply.github.com>
2024-10-06 07:08:16 +0700

    02-first-get-method

--- my_api/urls.py
+++ my_api/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('menu-items/', views.menu_items, name='menu-items'),
+    path('menu-items', views.menu_items, name='menu-items'),
 ]
--- my_api/views.py
+++ my_api/views.py
@@ -1,3 +1,5 @@
+from django.db import IntegrityError
+from django.forms import model_to_dict
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from .models import MenuItems
@@ -9,3 +11,14 @@ def menu_items(request):
     if request.method == 'GET':
         list_book = MenuItems.objects.all().values()
         return JsonResponse({"books": list(list_book)})
+    elif request.method == 'POST':
+        title = request.POST.getlist('title')
+        price = request.POST.getlist('price')
+        featured = request.POST.getlist('featured')
+        menu_item = MenuItems(title=title, price=price, featured=featured)
+        try:
+            menu_item.save()
+        except IntegrityError:
+            return JsonResponse({'error': 'true', 'message': 'required field missing'}, status=400)
+
+        return JsonResponse(model_to_dict(menu_item), status=201)
