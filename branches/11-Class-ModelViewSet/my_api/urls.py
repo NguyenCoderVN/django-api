f15c57ddcb7f13b4ebfe6b44b5be470b32221c13
NguyenCoderVN <41292507+NguyenCoderVN@users.noreply.github.com>
2024-10-09 12:14:36 +0700

    11-Class-ModelViewSet

---
+++ branches/11-Class-ModelViewSet/my_api/urls.py
@@ -0,0 +1,6 @@
+from rest_framework.routers import SimpleRouter
+from . import views
+
+router = SimpleRouter(trailing_slash=False)
+router.register('menu-items', views.MenuItemsView, basename='menu-items')
+urlpatterns = router.urls
