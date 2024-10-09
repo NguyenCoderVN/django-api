c061c4ad657ebee029182b9b4b66478d8873a0a3
NguyenCoderVN <41292507+NguyenCoderVN@users.noreply.github.com>
2024-10-09 09:10:16 +0700

    05-put-patch-delete-method-update

--- my_api/views.py
+++ my_api/views.py
@@ -4,51 +4,49 @@ from django.core.exceptions import ValidationError
 from django.forms import model_to_dict
 from .models import MenuItems
 from rest_framework.response import Response
-from rest_framework.decorators import APIView
+from rest_framework.decorators import APIView, api_view
 
 
-class MenuItemList(APIView):
+@api_view(['GET', 'POST'])
+def menu_items(request):
 
-    @staticmethod
-    def get(request):
-        items = MenuItems.objects.all().values()
+    match request.method:
+        case 'GET':
+            items = MenuItems.objects.all().values()
+            return Response({"books": list(items)})
 
-        return Response({"books": list(items)})
+        case 'POST':
+            try:
+                post_dict = json.loads(request.body)
+                title = post_dict["title"]
+                price = post_dict["price"]
+                featured = post_dict["featured"]
+                item = MenuItems(title=title, price=price, featured=featured)
+                item.save()
+            except JSONDecodeError as e:
+                return Response({"Json payload error": str(e)})
+            except KeyError:
+                return Response(
+                    data={'message': f'Json payload must have all fields'})
+            except ValidationError as e:
+                return Response({e.code: f'{e}'})
 
-    @staticmethod
-    def post(request):
-        try:
-            post_dict = json.loads(request.body)
-            title = post_dict["title"]
-            price = post_dict["price"]
-            featured = post_dict["featured"]
-            item = MenuItems(title=title, price=price, featured=featured)
-            item.save()
-        except JSONDecodeError as e:
-            return Response({"Json payload error": str(e)})
-        except KeyError:
-            return Response(
-                data={'message': f'Json payload must have all fields'})
-        except ValidationError as e:
-            return Response({e.code: f'{e}'})
-
-        return Response(model_to_dict(item), status=201)
+            return Response(model_to_dict(item), status=201)
 
 
-class MenuItem(APIView):
+@api_view(['GET', 'POST'])
+def menu_item(request):
 
-    @staticmethod
-    def get(request, pk):
-        try:
-            item = MenuItems.objects.get(pk=pk)
-            dict_items = model_to_dict(item)
-        except MenuItems.DoesNotExist as e:
-            return Response({'message': f'{e}'}, status=500)
+    match request.method:
+        case 'GET':
+            try:
+                item = MenuItems.objects.get(pk=pk)
+                dict_items = model_to_dict(item)
+            except MenuItems.DoesNotExist as e:
+                return Response({'message': f'{e}'}, status=500)
 
-        return Response(dict_items, status=200)
-
-    @staticmethod
-    def put(request, pk):
+            return Response(dict_items, status=200)
+    case 'PUT':
         try:
             item = MenuItems.objects.get(pk=pk)
             dict_put = json.loads(request.body)
@@ -73,22 +71,21 @@ class MenuItem(APIView):
             {f"Successfully update id {pk}": model_to_dict(item)},
             status=201)
 
-    @staticmethod
-    def patch(request, pk):
-        try:
-            item = MenuItems.objects.get(pk=pk)
-            dict_patch = json.loads(request.body)
-            for key, value in dict_patch.items():
-                setattr(item, key, value)
-            item.save()
-        except MenuItems.DoesNotExist as e:
-            return Response({'message': f'{e}'}, status=500)
-        except JSONDecodeError as e:
-            return Response({str(e)}, status=500)
+        case 'PACTH':
+                try:
+                    item = MenuItems.objects.get(pk=pk)
+                    dict_patch = json.loads(request.body)
+                    for key, value in dict_patch.items():
+                        setattr(item, key, value)
+                    item.save()
+                except MenuItems.DoesNotExist as e:
+                    return Response({'message': f'{e}'}, status=500)
+                except JSONDecodeError as e:
+                    return Response({str(e)}, status=500)
 
-        return Response(
-            {f"Successfully partial update id {pk}": model_to_dict(item)},
-            status=201)
+                return Response(
+                    {f"Successfully partial update id {pk}": model_to_dict(item)},
+                    status=201)
 
     @staticmethod
     def delete(request, pk):
