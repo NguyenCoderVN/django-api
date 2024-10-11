58f103d1a6204b1496d618347badbe05ec284c0d
NguyenCoderVN <41292507+NguyenCoderVN@users.noreply.github.com>
2024-10-11 12:20:38 +0700

    1402Quickstart

---
+++ refresh.ps1
@@ -0,0 +1,16 @@
+If (Test-Path db.sqlite3) {
+    Remove-Item db.sqlite3
+     Write-Host "db.sqlite3 removed"
+
+}
+If (Test-Path tutorials/quickstart/migrations) {
+    Remove-Item tutorials/quickstart/migrations -Recurse -Force -Confirm:$false
+    Write-Host "migrations removed"
+}
+python manage.py makemigrations
+python manage.py migrate
+$env:DJANGO_SUPERUSER_USERNAME="admin"
+$env:DJANGO_SUPERUSER_PASSWORD="books@123!"
+$env:DJANGO_SUPERUSER_EMAIL="test@gmail.com"
+python manage.py createsuperuser --noinput
+python manage.py runserver
---
+++ run.ps1
@@ -0,0 +1,1 @@
+http -a admin:books@123! http://127.0.0.1:8000/users/
