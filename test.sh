799a5ff12a8d49ac36d012be61783aa77d1260ea
NguyenCoderVN <nguyenmanhtriet@gmail.com>
2024-10-11 17:43:16 +0700

    1402Tutorial-4-Authentication-Permissions

--- test.sh
+++ test.sh
@@ -1,5 +1,2 @@
-http http://127.0.0.1:8000/snippets/ Accept:application/json  # Request JSON
-http http://127.0.0.1:8000/snippets/ Accept:text/html         # Request HTML
-http http://127.0.0.1:8000/snippets.json  # JSON suffix
-http http://127.0.0.1:8000/snippets.api   # Browsable API suffix
-http --form POST http://127.0.0.1:8000/snippets/ code="print(123)"
+http -a admin:books@123! POST http://127.0.0.1:8000/snippets/ code="print(789)"
+http http://127.0.0.1:8000/snippets/
