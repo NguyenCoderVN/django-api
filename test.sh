118a0d0a0b8e70afde73ee681224dc9bac42d2b6
NguyenCoderVN <nguyenmanhtriet@gmail.com>
2024-10-11 15:49:40 +0700

    1402Tutorial-2-Requests-and-Responses

--- test.sh
+++ test.sh
@@ -1,2 +1,5 @@
-http http://127.0.0.1:8000/snippets/ --unsorted
-http http://127.0.0.1:8000/snippets/2/ --unsorted
+http http://127.0.0.1:8000/snippets/ Accept:application/json  # Request JSON
+http http://127.0.0.1:8000/snippets/ Accept:text/html         # Request HTML
+http http://127.0.0.1:8000/snippets.json  # JSON suffix
+http http://127.0.0.1:8000/snippets.api   # Browsable API suffix
+http --form POST http://127.0.0.1:8000/snippets/ code="print(123)"
