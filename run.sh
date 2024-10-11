c99816172c37b5378031f48a5833ffc698ff48a5
NguyenCoderVN <nguyenmanhtriet@gmail.com>
2024-10-11 15:37:41 +0700

    1402Tutorial-1-Serialization

---
+++ run.sh
@@ -0,0 +1,7 @@
+python manage.py makemigrations
+python manage.py migrate
+export DJANGO_SUPERUSER_USERNAME=admin
+export DJANGO_SUPERUSER_PASSWORD=books@123!
+export DJANGO_SUPERUSER_EMAIL=test@gmail.com
+python manage.py createsuperuser --noinput
+python manage.py runserver
---
+++ test.sh
@@ -0,0 +1,2 @@
+http http://127.0.0.1:8000/snippets/ --unsorted
+http http://127.0.0.1:8000/snippets/2/ --unsorted
