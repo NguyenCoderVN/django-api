f4104112fa62487367071800b3652a348eaad6b5
NguyenCoderVN <nguyenmanhtriet@gmail.com>
2024-10-13 09:04:18 +0700

    16-02-Djoser

--- my_auth/views.py
+++ my_auth/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from rest_framework import viewsets
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
+
+from my_auth.serializers import UserSerializer
 
 
 # Create your views here.
@@ -12,3 +16,7 @@ def auth_message(request):
         return Response("Manage message here")
     else:
         return Response("You are not allowed to manage this message")
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
